f14dc80ae1aec0a266cf38593270855cfaf50d45
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-03 23:32:11 -0500

    build: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+
+const styleLi = (bg = "#456") => ({
+  backgroundColor: bg,
+  color: "#fff"
+})
+
+const Li = ({ children }) => {
+  <li style={styleLi("#582")} className="class-li">{children}</li>
+}
+
+const arr = [
+  "First element",
+  "Second element",
+  "Third element"
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {arr.map(el => <p key={el}>{el}</p>)}
+      <Button onClick={(e) => console.log('clicked', e)}>Soy un boton</Button>
     </div>
   );
 }
