9c2fcb451affe877d03a874cf47822d972fc3d8c
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-04 14:09:32 -0500

    feat: example shopping cart

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
-import { Component } from "react";
-
-class Input extends Component {
-
-  render() {
-    return (
-      <input
-        value={this.props.value}
-        onChange={this.props.onChange}
-      />
-    )
-  }
-}
-
+import { Component } from 'react'
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
 
 class App extends Component {
-
   state = {
-    name: '',
-    lastName: ''
+    productos: [
+      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
+      { name: 'Arbejas', price: 2500, img: '/productos/arbejas.jpg' },
+      { name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg' },
+    ],
+    carro: [],
+    esCarroVisible: false,
   }
 
-  updateValues = (prop, value) => {
-    this.setState({ [prop]: value })
+  agregarAlCarro = (producto) => {
+    const { carro } = this.state
+    if (carro.find(x => x.name === producto.name)) {
+      const newCarro = carro.map(x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+      return this.setState({ carro: newCarro })
+    }
+    return this.setState({
+      carro: this.state.carro.concat({
+        ...producto,
+        cantidad: 1,
+      })
+    })
+  }
+
+  mostrarCarro = () => {
+    if(!this.state.carro.length) {
+      return
+    }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible })
   }
 
   render() {
+    const { esCarroVisible } = this.state
+    
     return (
-      <p>
-        Full name: {this.state.name + ' ' + this.state.lastName}
-        <Input
-          value={this.state.name}
-          onChange={e => this.updateValues('name', e.target.value)}
+      <div>
+        <Navbar
+          carro={this.state.carro}
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
         />
-        <Input
-          value={this.state.lastName}
-          onChange={e => this.updateValues('lastName', e.target.value)}
-        />
-      </p>
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
+        </Layout>
+      </div>
     )
   }
 }
