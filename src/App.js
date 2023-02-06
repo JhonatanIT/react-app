106513db8aeec375c72735a60d098120534ad99e
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-05 19:42:13 -0500

    feat: useFormik and Formik component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import { useState } from 'react'
-import Card from './components/Card'
-import Container from './components/Container'
-import UserForm from './components/UserForm'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import TextInput from './components/TextInput'
+import Checkbox from './components/Checkbox'
+import Radio from './components/Radio'
+import Select from './components/Select'
 
-function App() {
-  const [usuarios, setUsuarios] = useState([])
+const validate = (values) => {
 
-  const submit = usuario => {
-    setUsuarios([
-      ...usuarios,
-      usuario,
-    ])
+  const errors = {}
+
+  if(!values.name) {
+    errors.name = 'Requerido'
+  } else if (values.name.length < 5) {
+    errors.name = 'El nombre es muy corto'
   }
 
-  console.log(usuarios)
-  return (
-    <div style={{ marginTop: '15%' }}>
-    <Container>
-      <Card>
-        <div style={{ padding: 20 }}>
-          <UserForm submit={submit} />
-        </div>
-      </Card>
-      <Card>
-        <ul>
-          {usuarios.map(x =>
-          <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>)}
-        </ul>
-      </Card>
-    </Container>
-    </div>
-  )
+  if(!values.lastname) {
+    errors.lastname = 'Requerido'
+  } else if (values.lastname.length < 5) {
+    errors.lastname = 'El apellido es muy corto'
+  }
+
+  if(!values.radio) {
+    errors.radio = 'Requerido'
+  }
+
+  return errors
 }
 
-export default App
+function App() {
+  return (
+    <Formik
+      initialValues={{ name: '', lastname: '', email: '', chancho: '', radio: '' }}
+      validate={validate}
+      onSubmit={values => console.log(values)}
+    >
+      <Form>
+        <TextInput name="name" label="Nombre" />
+        <br />
+        <TextInput name="lastname" label="Apellido" />
+        <br />
+        <TextInput name="email" label="Correo" />
+        <Select label="Tipo de chancho" name="chancho">
+          <option value="">Seleccione chancho</option>
+          <option value="felipe">Felipe</option>
+          <option value="chanchitofeliz">Chanchito Feliz</option>
+          <option value="chanchitotriste">Chanchito Triste</option>
+        </Select>
+        <Checkbox name="accept">
+          Aceptar términos y condiciones
+        </Checkbox>
+        <Radio name="radio" value="chanchito1" label="chanchito 1" />
+        <Radio name="radio" value="chanchito2" label="chanchito 2" />
+        <Radio name="radio" value="chanchito3" label="chanchito 3"/>
+        <ErrorMessage name="radio" />
+        <button type="submit">Enviar</button>
+      </Form>
+    </Formik>
+  );
+}
+
+export default App;
