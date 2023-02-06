62325394bd6b440cf34151800bc167547b276d59
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-05 21:41:08 -0500

    feat: search images

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import TextInput from './components/TextInput'
-import Checkbox from './components/Checkbox'
-import Radio from './components/Radio'
-import Select from './components/Select'
+import { useState } from 'react'
+import { Formik, Form, Field } from 'formik'
+import './header.css'
+import './article.css'
+import './content.css'
 
-const validate = (values) => {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  console.log(photos)
+  const open = (url) => window.open(url)
 
-  const errors = {}
-
-  if(!values.name) {
-    errors.name = 'Requerido'
-  } else if (values.name.length < 5) {
-    errors.name = 'El nombre es muy corto'
-  }
-
-  if(!values.lastname) {
-    errors.lastname = 'Requerido'
-  } else if (values.lastname.length < 5) {
-    errors.lastname = 'El apellido es muy corto'
-  }
-
-  if(!values.radio) {
-    errors.radio = 'Requerido'
-  }
-
-  return errors
-}
-
-function App() {
   return (
-    <Formik
-      initialValues={{ name: '', lastname: '', email: '', chancho: '', radio: '' }}
-      validate={validate}
-      onSubmit={values => console.log(values)}
-    >
-      <Form>
-        <TextInput name="name" label="Nombre" />
-        <br />
-        <TextInput name="lastname" label="Apellido" />
-        <br />
-        <TextInput name="email" label="Correo" />
-        <Select label="Tipo de chancho" name="chancho">
-          <option value="">Seleccione chancho</option>
-          <option value="felipe">Felipe</option>
-          <option value="chanchitofeliz">Chanchito Feliz</option>
-          <option value="chanchitotriste">Chanchito Triste</option>
-        </Select>
-        <Checkbox name="accept">
-          Aceptar términos y condiciones
-        </Checkbox>
-        <Radio name="radio" value="chanchito1" label="chanchito 1" />
-        <Radio name="radio" value="chanchito2" label="chanchito 2" />
-        <Radio name="radio" value="chanchito3" label="chanchito 3"/>
-        <ErrorMessage name="radio" />
-        <button type="submit">Enviar</button>
-      </Form>
-    </Formik>
+    <>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID qvy2y7fRCE_mIZrDJ8fevnKO8Mm29ZYhiXQSRq70mOw'
+              }
+            })
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
+        >
+          <Form>
+            <Field name='search' />
+          </Form>
+        </Formik>
+      </header>
+      <div class="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.description} />
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
