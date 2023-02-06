4b7637cc70ca0321cc384cb100566bb7f0671e72
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-06 00:23:49 -0500

    feat: using styled-components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,93 @@
-import { useState } from 'react'
-import { Formik, Form, Field } from 'formik'
-import './header.css'
-import './article.css'
-import './content.css'
+import styled, { keyframes } from 'styled-components'
 
-const App = () => {
-  const [photos, setPhotos] = useState([])
-  console.log(photos)
-  const open = (url) => window.open(url)
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
 
+const Content = styled.div`
+  padding: 20px 25px;
+`
+
+const Button = styled.button`
+  transition: box-shadow 0.2s ease;
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+
+  &:hover {
+    box-shadow: 1px 2px 5px rgb(0,0,0,0.3);
+  }
+
+  &.secondary {
+    background-color: blue;
+    border: solid 2px blue;
+    color: white;
+  }
+  .info {
+    font-size: 28px;
+  }
+`
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`
+
+const Link = ({ className, ...props }) => {
+  return <a className={className} {...props}></a>
+}
+
+const StyledLink = styled(Link)`
+  color: blue;
+`
+
+const Input = styled.input.attrs(props => ({
+  type: 'text',
+  color: props.color || 'red'
+}))`
+  font-size: 20px;
+  border: 1px solid red;
+  color: ${props => props.color};
+`
+
+const Password = styled(Input).attrs({
+  type: 'password'
+})``
+
+const girar = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const Rotar = styled.div`
+  display: inline-block;
+  animation: ${girar} 2s linear infinite;
+`
+
+function App() {
   return (
-    <>
-      <header>
-        <Formik
-          initialValues={{ search: '' }}
-          onSubmit={async values => {
-            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
-              headers: {
-                'Authorization': 'Client-ID qvy2y7fRCE_mIZrDJ8fevnKO8Mm29ZYhiXQSRq70mOw'
-              }
-            })
-            const data = await response.json()
-            setPhotos(data.results)
-          }}
-        >
-          <Form>
-            <Field name='search' />
-          </Form>
-        </Formik>
-      </header>
-      <div class="container">
-        <div className="center">
-          {photos.map(photo =>
-            <article key={photo.id} onClick={() => open(photo.links.html)}>
-              <img src={photo.urls.regular} alt={photo.description} />
-              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
-            </article>
-          )}
-        </div>
-      </div>
-    </>
+    <Content>
+      <P>Hola soy un parrafo!</P>
+      <Button>Enviar<p className="info">Info</p></Button>
+      <Button primary>Enviar</Button>
+      <Button className="secondary">Enviar</Button>
+      <BlockButton primary as="a" href="#">Enviar</BlockButton>
+      <BlockButton primary>Enviar</BlockButton>
+      <Link>Link</Link>
+      <StyledLink>Link con estilo</StyledLink>
+      <Input />
+      <Password />
+      <br />
+      <Rotar>Estoy girando</Rotar>
+    </Content>
   );
 }
 
