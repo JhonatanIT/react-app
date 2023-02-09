8a0502594fab293238e9695f17e70c30dad8108a
JhonatanIT <jhonatan.ibatac@gmail.com>
2023-02-08 20:40:41 -0500

    fix: update routes react

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@
   In nested routes use * like: '/portafolio/*'
 */
 
-import { Route, Link, useParams, Routes } from 'react-router-dom'
+import { Route, Routes, Link, Navigate, NavLink, useParams, useLocation, useNavigate } from 'react-router-dom'
+
+//Navigate: allow to redirect an specific path
+//NavLink: indicate if the actual link is activated
+
+//Access query parameters like ?value=2
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 
 const Proyecto = () => {
   const params = useParams()// no devuelve la ruta del proyecto, devuelve la ruta del componente!!
@@ -12,27 +21,26 @@ const Proyecto = () => {
   console.log({ params })
   return (
     <p>
-      proyecto...
+      Proyecto: {params.id}
     </p>
   )
 }
 
 const Portafolio = () => {
   const params = useParams() // mostrar que no se obtienen los parametros de la ruta, si no que del componente siempre que este acepte con el componente de Route
-  console.log({ params }, 'lala')
 
   return (
     <div>
       <h1>Portafolio</h1>
       <ul>
         <li>
-          <Link to={`proyecto-1`}>Proyecto 1</Link>
+          <NavLink className={({ isActive }) => isActive ? "activado" : undefined} exact to={`proyecto-1`}>Proyecto 1</NavLink>
         </li>
         <li>
-          <Link to={`proyecto-2`}>Proyecto 2</Link>
+          <NavLink style={({ isActive }) => isActive ? { fontSize: "20px" } : undefined} exact to={`proyecto-2`}>Proyecto 2</NavLink>
         </li>
         <li>
-          <Link to={`proyecto-3`}>Proyecto 3</Link>
+          <NavLink exact to={`proyecto-3`}>Proyecto 3</NavLink>
         </li>
       </ul>
       <div>
@@ -44,27 +52,51 @@ const Portafolio = () => {
     </div>
   )
 }
+
 function App() {
+
+  const query = useQuery()
+  console.log(query.get('value'))
+
+  const navigate = useNavigate()
+  const forward = () => {
+    //navigate.goForward()
+    navigate(1)
+  }
+
+  const back = () => {
+    //navigate.goBack()
+    navigate(-1)
+  }
+
+  const home = () => {
+    navigate("/")
+  }
+
   return (
     <div>
       <nav>
         <ul>
           <li>
-            <Link to="/">Inicio</Link>
+            <NavLink exact to="/">Inicio</NavLink>
           </li>
           <li>
-            <Link to="/portafolio">Portafolio</Link>
+            <NavLink exact to="/portafolio">Portafolio</NavLink>
           </li>
           <li>
-            <Link to="/perfil">Perfil</Link>
+            <NavLink exact to="/perfil">Perfil</NavLink>
           </li>
         </ul>
       </nav>
       <section>
+        <button onClick={back}>Atras</button>
+        <button onClick={forward}>Adelante</button>
+        <button onClick={home}>Home</button>
         <Routes>
           <Route path="/portafolio/*" element={<Portafolio />} />
           <Route path="/perfil" element={<h1>Perfil</h1>} />
           <Route path="/" element={<h1>Inicio</h1>} />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </section>
     </div>
